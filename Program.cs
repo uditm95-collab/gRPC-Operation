67919aec9907f3bcbd80495b8a4033d711576aeb
uditm95-collab <uditm95@gmail.com>
2025-08-15 20:29:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+using Grpc.Net.Client;
+using GrpcDemo;
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Udit" });
+Console.WriteLine(reply.Message);
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
